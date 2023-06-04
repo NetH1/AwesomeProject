91a2cdcb3fde4b5f159b6ce745c7da7193de044d
NetH1 <youtube_17@mail.ru>
2023-06-04 16:19:27 +0600

    lastupdate

--- App.js
+++ App.js
@@ -1,112 +1,66 @@
-import React from "react";
+import React, { useEffect, useState, useRef } from "react";
 import {
-  Alert,
-  Button,
-  Image,
-  ImageBackground,
-  Keyboard,
+  Animated,
+  ActivityIndicator,
+  Dimensions,
+  FlatList,
   Text,
-  TextInput,
   View,
+  SafeAreaView,
   StyleSheet
 } from "react-native";
-import Calculator from "./assets/components/Calculator";
-import Galery from "./assets/Galery";
+import ListItem from "../components/ListItem";
 
-//export default function App() {
+const d = new Date();
+let hour = d.getHours();
+
+const { width, height } = Dimensions.get("screen");
+
 const App = () => {
-  const onButtonPress = () => {
-    setIsLogging(true);
-    if (useFirebase) {
-    } else {
-      setTimeout(() => {
-        if (email == "aj_pktl@mail.ru" && password == "asd123") {
-          setIsLogged(true);
-        }
-        setIsLoggingIn(false);
-      }, 1500);
-    }
-    // alert("Hello");
-    // Alert.alert("Error");
-  };
-  const [email, setEmail] = React.useState('');
-  const [password, setPassword] = React.useState('');
-  const [isLogged, setIsLogged] = React.useState(false);
+  const scrollX = useRef(new Animated.Value(0)).current;
+
+  const locations = [
+    { key: 1, name: "Almaty", lat: 43.26, lon: 76.93 },
+    { key: 2, name: "Astana", lat: 51.18, lon: 71.45 }
+  ];
+
+  const viewableItemsChanged = useRef(({ viewableItems }) => {
+    //console.log(viewableItems);
+    //setCurrentIndex(viewableItems[0].index);
+  }).current;
+  const viewConfig = useRef({ viewAreaCoveragePercentThreshold: 60 }).current;
 
   return (
-    <View style={styles.app}>
-      {/* <ImageBackground
-        style={styles.main_background}
-        source={{
-          uri:
-            "https://phonoteka.org/uploads/posts/2022-02/1643998266_1-phonoteka-org-p-fon-dlya-prezentatsii-nebo-s-oblakami-1.jpg"
-        }}
-      >
-        <Image
-          style={styles.main_image}
-          source={{
-            uri: "https://img.icons8.com/?size=512&id=108639&format=png"
-          }}
-        />
-        <Text style={styles.main_text}  onPress={Keyboard.dismiss}>Hello React Native</Text>
-        <View style={styles.inputs_wrap}>
-          <TextInput
-          inputMode="email"
-            style={styles.user_email}
-            placeholder="user@mail.com"
-            placeholderTextColor="#aaa"
-            onChangeText={setEmail}
-            value={email}
-            onBlur={() => {
-              console.log("text input blur");
-            }}
-          />
-          <TextInput
-          secureTextEntry={true}
-            style={styles.user_email}
-            placeholder="user@mail.com"
-            onChangeText={setPassword}
-            value={password}
-            placeholderTextColor="#aaa"
-          />
-        </View>
-        <Button title="Log In" onPress={onButtonPress} />
-      </ImageBackground> */}
-      {/* <Calculator /> */}
-      <Galery />
-    </View>
+    <SafeAreaView style={styles.app}>
+      <Animated.FlatList
+        data={locations}
+        renderItem={({ item, index }) => (
+          <ListItem item={item} index={index} scrollX={scrollX} />
+        )}
+        horizontal={true}
+        pagingEnabled={true}
+        showsHorizontalScrollIndicator={false}
+        //!!!только для web
+        contentContainerStyle={{ width, height }}
+        bounces={false}
+        scrollEventThrottle={32}
+        onScroll={Animated.event(
+          [{ nativeEvent: { contentOffset: { x: scrollX } } }],
+          { useNativeDriver: false }
+        )}
+        onViewableItemsChanged={viewableItemsChanged}
+        viewabilityConfig={viewConfig}
+      />
+    </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
   app: {
-    flex: 1
-  },
-  main_background: {
     flex: 1,
     justifyContent: "center",
     alignItems: "center"
-  },
-  main_image: {
-    height: 248,
-    width: 248
-  },
-  main_text: {
-    fontSize: 48,
-    textAlign: "center"
-  },
-  inputs_wrap: {
-    padding: 12,
-    width: 450,
-    alignItems: "center"
-  },
-  user_email: {
-    borderRadius: 6,
-    paddingVertical: 12,
-    paddingHorizontal: 18,
-    backgroundColor: "white",
-    color: "#333",
-    width: "75%"
+    // backgroundColor: "#aaa"
   }
 });
 
